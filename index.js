8d8906f0f8f14f292f0449e880b3b8d62c562616
PrafulKumarRajput1411 <84839942+PrafulKumarRajput1411@users.noreply.github.com>
2024-03-19 22:34:00 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const cookieParser = require('cookie-parser')
 const cors = require('cors')
+
 const db = require('./config/database')
 const app = express();
 const PORT = 8001;
